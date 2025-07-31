3249fb6b1c260135ae4cd340f28c96070e77d3e3
caues <caues@SCHMITZNOT>
2025-07-31 11:41:54 -0300

    Implementada camada de Infraestrutura em Arquivos

---
+++ ControleDeBar.Infraestrutura.Arquivos/ModuloGarcom/RepositorioGarcom.cs
@@ -0,0 +1,6 @@
+﻿using ControleDeBar.Dominio.ModuloGarcom;
+using ControleDeBar.Infraestrutura.Memoria.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.Memoria.ModuloGarcom;
+
+public class RepositorioGarcom : RepositorioBase<Garcom>;
---
+++ ControleDeBar.Infraestrutura.Arquivos/ModuloMesa/RepositorioMesa.cs
@@ -0,0 +1,6 @@
+﻿using ControleDeBar.Dominio.ModuloMesa;
+using ControleDeBar.Infraestrutura.Memoria.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.Memoria.ModuloMesa;
+
+public class RepositorioMesa : RepositorioBase<Mesa>;
